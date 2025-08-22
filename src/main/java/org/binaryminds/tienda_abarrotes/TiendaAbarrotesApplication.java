a0298489e5bf1661d53dab8d4913c910d410bc13
wflorian <wflorian-2024392@kinal.edu.gt>
2025-08-22 16:00:05 -0600

    creación del proyecto

---
+++ src/main/java/org/binaryminds/tienda_abarrotes/TiendaAbarrotesApplication.java
@@ -0,0 +1,13 @@
+package org.binaryminds.tienda_abarrotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAbarrotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAbarrotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/binaryminds/tienda_abarrotes/TiendaAbarrotesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binaryminds.tienda_abarrotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAbarrotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
